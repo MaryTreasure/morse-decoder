e3d3483d464326317ab88d953061e3f5372e0405
MaryTreasure <mari199191@mail.ru>
2024-08-04 17:52:14 +0300

    feat: implement morse decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let splitExpr = [];
+
+    for (let i = 0; i < expr.length; i +=10) {
+      splitExpr.push(expr.slice(i, i + 10))
+    }
+  
+    let resultArr = splitExpr.map((item, index) => {
+      let partialArr = []
+      
+      for(i = 0; i < item.length; i+=2) {
+        if(item[0] == 0 || item[0] == 1) {
+          partialArr.push(item.slice(i, i + 2))
+        } else {
+          partialArr.push(item);
+          break;
+        }
+          }
+          return partialArr
+    })
+  
+    let result = resultArr.map (item => {
+      
+      let resultItem = [] 
+      for(let el of item) {
+        if (el === '10') {
+          resultItem.push('.')
+        } else if (el === '11') {
+          resultItem.push('-')
+        } else if (el === '**********') {
+          resultItem.push(el)
+        }
+  
+      }
+      return resultItem.join('')
+    })
+  
+    let resultStr = result.map((item) => {
+      if (item === '**********') {
+        return ' '
+      } else {
+        return MORSE_TABLE[item]
+      }
+    })
+    return resultStr.join('')
 }
 
 module.exports = {
